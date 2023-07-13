66b36450173f64e1d60db90d5baa1df408c61112
Guillermo Lescano <glescano@vates.com>
2023-07-13 01:22:24 -0300

    Armando Formulario

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Fragment } from 'react'
+import {MARCAS, YEARS, PLANES} from '../constants'
+
+const Formulario = () => {
+  return (
+    <>
+    <form>
+        <div className='my-5'>
+            <label className='block mb-3 font-bold text-gray-400 '>Marca</label>
+            <select name="marca" className='w-full p-3 bg-white border border-gray-200'>
+                <option value="">--Seleccione una Marca--</option>
+                {MARCAS.map(marca =>
+                    <option
+                        key={marca.id}
+                        value={marca.id}
+                    >
+                        {marca.nombre}
+                    </option>
+                )}
+            </select>
+        </div>
+        <div className='my-5'>
+            <label className='block mb-3 font-bold text-gray-400 '>Año</label>
+            <select name="marca" className='w-full p-3 bg-white border border-gray-200'>
+                <option value="">--Seleccione un Año--</option>
+                {YEARS.map(year =>
+                    <option
+                        key={year}
+                        value={year}
+                    >
+                        {year}
+                    </option>
+                )}
+            </select>
+        </div>
+        <div className='my-5'>
+            <label className='block mb-3 font-bold text-gray-400 '>Selecciona un Plan</label>
+            <div className='flex gap-3 items-center'>
+                {PLANES.map(plan =>(
+                    <Fragment key={plan.id}>
+                        <label>{plan.nombre}</label>
+                        <input type='radio' name='plan' value={plan.id}></input>
+                    </Fragment>
+                ))}
+            </div>
+        </div>
+        <input type="submit" className='w-full bg-indigo-500 hover:bg-indigo-600 transition-colors 
+        text-white cursor-pointer p-3 uppercase font-bold' value='Cotizar' />
+    </form>
+    </>
+  )
+}
+
+export default Formulario
