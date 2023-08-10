2738e3bbd3e9f9351729eb433947b18d67a55b80
Guillermo Lescano <glescano@vates.com>
2023-08-10 01:26:22 -0300

    Añadiendo informaicon al Resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,10 +1,23 @@
 import useCotizador from '../hooks/useCotizador'
+import { MARCAS, PLANES } from '../constants'
 
 export const Resultado = () => {
-    const {resultado} = useCotizador()
+    const {resultado, datos} = useCotizador()
+    const {marca, year, plan} = datos
+
+    const [nombreMarcas] = MARCAS.filter(m => m.id ===Number(marca)) //lo extrae como un objeto su ponemos corchetes
+
     if( resultado === 0) return null
   return (
-    
-    <div>{resultado}</div>
+    <div className='bg-gray-100 text-center mt-5 p-5 shadow '>
+      <h2 className='text-gray-600 font-black text-3xl'>
+        Resumen
+      </h2>
+
+      <p className='my-2 '>
+        <span className='font-bold'>Marca:</span>
+        {nombreMarcas.nombre}
+      </p>
+    </div>
   )
 }
