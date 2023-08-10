87274144ef56d5db0b0d684affb4b00f5c204b59
Guillermo Lescano <glescano@vates.com>
2023-08-10 01:54:30 -0300

    cambios

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -14,7 +14,7 @@ export const Resultado = () => {
   return (
     <div className='bg-gray-100 text-center mt-5 p-5 shadow '>
       <h2 className='text-gray-600 font-black text-3xl'>
-        Resumen
+        Resumen...
       </h2>
       <p className='my-2 '>
         <span className='font-bold'>Marca:</span>
